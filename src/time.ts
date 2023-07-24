d61aba8c54fd145104cdce53300fc4648d1ff502
Jonster5 <onlyaccelerating@gmail.com>
2023-07-24 17:56:53 -0400

    system timer utils

--- src/time.ts
+++ src/time.ts
@@ -12,6 +12,12 @@ export class Time extends Resource {
 	}
 }
 
+export class SystemTimer extends Resource {
+	constructor(public remaining: number) {
+		super();
+	}
+}
+
 function startTime(ecs: ECS) {
 	const time = ecs.getResource(Time)!;
 
@@ -29,4 +35,25 @@ function updateTime(ecs: ECS) {
 
 export function TimePlugin(ecs: ECS) {
 	ecs.insertResource(new Time()).addStartupSystem(startTime).addMainSystem(updateTime);
+}
+
+/**
+ * @returns `true` if the current system's timer still has time left, `false` if the system is ready or if there is no system timer available
+ */
+export function checkTimer(ecs: ECS) {
+	if (!ecs.hasLocalResource(SystemTimer)) ecs.insertLocalResource(new SystemTimer(0));
+
+	const timer = ecs.getLocalResource(SystemTimer)!;
+	timer.remaining -= ecs.getResource(Time)!.delta;
+
+	return timer.remaining > 0;
+}
+
+/**
+ * To be used in conjunction with `checkTimer`
+ * @param ecs A reference to the ECS
+ * @param delay The amount of time in milliseconds to wait before running this system again
+ */
+export function setTimer(ecs: ECS, delay: number) {
+	ecs.insertLocalResource(new SystemTimer(delay), true);
 }
