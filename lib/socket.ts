6b02b9ad745e9a17d16b9fee55aee9a71ae8791c
Jonster5 <onlyaccelerating@gmail.com>
2023-07-26 20:48:24 -0400

    yup

--- lib/socket.ts
+++ lib/socket.ts
@@ -146,6 +146,38 @@ export function decodeString(buffer: ArrayBuffer): string {
 	return String.fromCodePoint(...new Uint16Array(buffer));
 }
 
+export function stitch(...buffers: ArrayBuffer[]): ArrayBuffer {
+	const message = new Uint8Array(2 + buffers.length * 2 + buffers.reduce((a, b) => a + b.byteLength, 0));
+
+	const hlength = new Uint16Array([buffers.length]).buffer;
+	const psizes = new Uint16Array(buffers.map((b) => b.byteLength)).buffer;
+
+	message.set(new Uint8Array(hlength), 0);
+	message.set(new Uint8Array(psizes), 2);
+
+	let offset = hlength.byteLength + psizes.byteLength;
+	for (let i = 0; i < buffers.length; i++) {
+		message.set(new Uint8Array(buffers[i]), offset);
+		offset += buffers[i].byteLength;
+	}
+
+	return message.buffer;
+}
+
+export function unstitch(buffer: ArrayBuffer): ArrayBuffer[] {
+	const hlength = new Uint16Array(buffer)[0];
+	const psizes = new Uint16Array(buffer.slice(2, 2 + hlength * 2));
+
+	const data = new Array(psizes.length);
+
+	let offset = 2 + hlength * 2;
+	for (let i = 0; i < psizes.length; i++) {
+		data[i] = buffer.slice(offset, (offset += psizes[i]));
+	}
+
+	return data;
+}
+
 export function getSocket(ecs: ECS, label: string) {
 	return ecs.getResource(SocketManager)!.sockets.get(label);
 }
